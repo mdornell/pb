98619c20e035b854cb7d1caeb5c2c899d3994bf5
mdornell <87149031+mdornell@users.noreply.github.com>
2021-07-30 17:52:40 -0300

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,8 @@
+function teste(){
+	if(document.getElementById("sidebar").style.visibility == "hidden"){
+    document.getElementById("sidebar").style.visibility = "visible ";
+	}
+	else{
+	document.getElementById("sidebar").style.visibility = "hidden";	
+	}
+}
